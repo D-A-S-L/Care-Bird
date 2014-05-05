285ff16a8d489e1a9a1091a11581e7ee2d3cea2c
Chris Murphy <christoph2005@gmail.com>
2014-05-04 21:51:55 -0700

    A simple test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?   
-echo "Hello World";
+echo "Hello World</br>";
 $conn = pg_connect("host=ec2-54-225-136-187.compute-1.amazonaws.com    
 
     port=5432    
@@ -12,7 +12,10 @@ $conn = pg_connect("host=ec2-54-225-136-187.compute-1.amazonaws.com
 
 $sql = "SELECT * from ourgroup";   
 
-$result = pg_query($conn, $sql);   
+$result = pg_query($conn, $sql);
+
+foreach($result as $row)
+	print_r($row);
 
 $row = pg_fetch_object($result, 0);   
 
