3257c52d9dc9feabec3eac5b9dd5bc3a780fa12c
Chris Murphy <christoph2005@gmail.com>
2014-05-04 19:45:05 -0700

    Trying to add a simple test

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?print("Hello World");?>
+<?   $conn = pg_connect("host=ec2-54-225-136-187.compute-1.amazonaws.com    
+
+    port=5432    
+
+    dbname=d86fo56ie0kqjt    
+
+    user=yndbtfxmnwkcgi    
+
+    password=hiAXar8M6tn8OQNC1zrEDskrKO");   
+
+$sql = "SELECT * from ourgroup";   
+
+$result = pg_query($conn, $sql);   
+
+$row = pg_fetch_object($result, 0);   
+
+echo "Today is: " .$row;   
+
+?>
