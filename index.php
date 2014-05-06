20873f3e77e1a23c06cacaec27bfc44ae6aec74d
Chris Murphy <christoph2005@gmail.com>
2014-05-06 01:01:52 -0700

    Trying to make a json response

--- index.php
+++ index.php
@@ -19,13 +19,19 @@ if(empty($result))
 else
 	deliver_response(200, "Table 'ourgroup'", $result);
 
+while ($row = pg_fetch_row($result)) {
+  echo "Name: $row[0]<br/>\n
+		LastName: $row[1] <br/>\n
+		Coolness: $row[2] <br/>\n <br/>\n";
+}  
 function deliver_response($status, $status_message, $data){
 	header("HTTP/1.1 $status $status_message");
 	$response['status']=$status;
 	$response['status_message']=$status_message;
 	$response['data']=$data;
 	
-	$json_response=json_encode($response);
+	$responseArray=pg_fetch_all($result);
+	$json_response=json_encode($responseArray);
 	echo $json_response;
 }
 pg_close ($conn);
