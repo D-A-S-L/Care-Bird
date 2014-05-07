879ee1936539daa81e7f70507a4b50f74a3138d8
Chris Murphy <christoph2005@gmail.com>
2014-05-07 00:39:38 -0700

    Working on webservice

---
+++ dbconn.php
@@ -0,0 +1,13 @@
+<?   
+global $conn = pg_connect("host=ec2-54-225-136-187.compute-1.amazonaws.com    
+
+    port=5432    
+
+    dbname=d86fo56ie0kqjt    
+
+    user=yndbtfxmnwkcgi    
+
+    password=hiAXar8M6tn8OQNC1zrEDskrKO")
+	 or die("Could not connect");
+echo "Connected Successfully";
+?>
