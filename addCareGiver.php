1e89b7e9f90a9c5d25b18d467e0b47bc6e5352f4
Chris Murphy <christoph2005@gmail.com>
2014-05-07 03:42:05 -0700

    Working on webservice

--- addCareGiver.php
+++ addCareGiver.php
@@ -22,8 +22,7 @@ $authRow = pg_fetch_row($authResponse);
 if(empty($authRow))
 	deliver_response(200, "No User with that username", NULL);
 else{
-    $jr= json_encode($authRow);
-    echo $jr;
+    echo $authRow;
     if($crpass != $authRow['pass'])
 	   deliver_response(200, "Invalid password", NULL);
     else{
