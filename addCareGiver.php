c3b7898a027b0e4b05be5c928f798bc4aed72b78
Chris Murphy <christoph2005@gmail.com>
2014-05-07 03:47:19 -0700

    Working on webservice

--- addCareGiver.php
+++ addCareGiver.php
@@ -26,7 +26,7 @@ else if($crpass != $authRow[0])
 else{
     // User found and Pass was correct
     // So add the Token to the QRToken's table.    
-    $addTokenQuery="insert into QRToken values ('$crid', '$Token')";
+    $addTokenQuery="inserft into QRToken values ('$crid', '$Token')";
     $addTokenResponse=pg_query($conn,$addTokenQuery);
     deliver_response(200, "addToken Query Result", $addTokenResponse);
 }
