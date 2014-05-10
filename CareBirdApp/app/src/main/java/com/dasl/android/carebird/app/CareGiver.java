386182ac81ebd56dd70096772a6b27966fb84127
David Scianni <dnscianni@csupomona.edu>
2014-05-09 22:03:52 -0700

    Edited The QR reader and CG CR pages

---
+++ CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/CareGiver.java
@@ -0,0 +1,14 @@
+package com.dasl.android.carebird.app;
+
+/**
+ * Created by David on 5/9/2014.
+ */
+public class CareGiver {
+    private int phoneNumber;
+    private String userName;
+    private String name;
+    private int StartTime;
+    private int endTime;
+
+
+}
--- CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/CareReceiversActivity.java
+++ CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/CareReceiversActivity.java
@@ -2,7 +2,6 @@ package com.dasl.android.carebird.app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
@@ -13,63 +12,32 @@ import android.widget.Button;
 import android.widget.TextView;
 
 /**
- * Created by David on 4/30/14.
+ * Created by David on 5/9/2014.
  */
-public class MainMenuActivity extends Activity {
+public class CareReceiversActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu_activity);
+        setContentView(R.layout.care_receivers_activity);
 
-        // Font path
-        String fontPath = "fonts/APHont-Regular_q15c.ttf";
+        TextView tview1 = (TextView) findViewById(R.id.CareReceiver1Name);
+        tview1.setText("Name: " + getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("otherName", "Null"));
 
-        // text view label
-        TextView schedulesButton = (TextView) findViewById(R.id.Schedules);
-        TextView careGiversButton = (TextView) findViewById(R.id.CareGivers);
-        TextView settingsButton = (TextView) findViewById(R.id.QRcode);
+        TextView tview2 = (TextView) findViewById(R.id.CareReceiver1UserName);
+        tview2.setText("User Name: " + getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("otherUserName", "Null"));
 
-        // Loading Font Face
-        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
-
-        // Applying font
-        schedulesButton.setTypeface(tf);
-        careGiversButton.setTypeface(tf);
-        settingsButton.setTypeface(tf);
+        TextView tview3 = (TextView) findViewById(R.id.CareReceiver1Phone);
+        tview3.setText("Phone: " + getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("otherPhoneNumber", "tel:1111111111").substring(4, 7) + ") " + getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("otherPhoneNumber", "Null").substring(7, 10) + "-" + getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("otherPhoneNumber", "Null").substring(10));
 
         PhoneCallListener phoneListener = new PhoneCallListener();
         TelephonyManager telephonyManager = (TelephonyManager) this
                 .getSystemService(this.TELEPHONY_SERVICE);
         telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
 
-        final Button button0 = (Button) findViewById(R.id.Panic);
-        button0.setOnClickListener(new View.OnClickListener() {
+        final Button button = (Button) findViewById(R.id.CallButton2);
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent = new Intent(Intent.ACTION_CALL, Uri.parse(getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("otherPhoneNumber", "tel:1111111")));
-                startActivityForResult (myIntent, 0);
-            }
-        });
-
-        final Button button1 = (Button) findViewById(R.id.Schedules);
-        button1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), ErrorActivity.class);
-                startActivityForResult (myIntent, 0);
-            }
-        });
-
-        final Button button2 = (Button) findViewById(R.id.CareGivers);
-        button2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), CareGiversActivity.class);
-                startActivityForResult (myIntent, 0);
-            }
-        });
-
-        final Button button3 = (Button) findViewById(R.id.QRcode);
-        button3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), QRCodeActivity.class);
                 startActivityForResult (myIntent, 0);
             }
         });
