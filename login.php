15b76b64c5c284e844f95e957a722823fc39fc83
Chris Murphy <christoph2005@gmail.com>
2014-05-10 03:32:16 -0700

    Tried to add a login session key

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ header("Content-Type: application/json");
 $UName = $_GET["UName"];
 $Pass = $_GET["Pass"];
 $SessionKey = $_GET["SessionKey"];
-    
+    echo "before function";
 function notLoggedIn(){
 	deliver_response(200, "User is not logged in", false);
 }
@@ -18,10 +18,9 @@ if (empty($SessionKey) and (empty($UName) or empty($Pass)){
 	$query="select SessionKey from SessionKeys where SessionKey='SessionKey';";
 	$rTable=pg_query($conn,$query);
 	$row = pg_fetch_row($rTable);
-	if(empty($row))
+	if(empty($row)){
 		notLoggedIn();
-	else 
-		deliver_response(200, "SessionKey valid", true);
+	} else {deliver_response(200, "SessionKey valid", true);}
 } else if(!(empty($UName) or empty($Pass))){
 	$query="delete from SessionKeys where UName='$Uname';";
 	$rTable=pg_query($conn,$query);
@@ -29,7 +28,7 @@ if (empty($SessionKey) and (empty($UName) or empty($Pass)){
 	$query="insert into SessionKeys values ('$Uname','$SessionKey');";
 	$rTable=pg_query($conn,$query);
 	deliver_response(200, "Username not found", $SessionKey);
-} else deliver_response(200,"Invalid authentication", NULL);
+} else {deliver_response(200,"Invalid authentication", NULL);}
 
 pg_close($conn);
 ?>
