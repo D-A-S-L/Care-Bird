86b31d5ab920253385ad9135cf5572d9b73db577
Chris Murphy <christoph2005@gmail.com>
2014-05-10 05:58:25 -0700

    caredb.herokuapp.com/SessionKeys.php has been added so you can see new entries in the Session table

---
+++ SessionKeys.php
@@ -0,0 +1,13 @@
+<?php
+require 'dbconn.php';
+header("Content-Type: application/json");
+
+$sql = "SELECT * from SessionKeys";
+$result = pg_query($conn, $sql);
+
+if(empty($result))
+	deliver_response(200, "No entries in table 'SessionKeys'", NULL);
+else
+	deliver_response(200, "Table 'SessionKeys'", $result);
+pg_close ($conn);
+?>
