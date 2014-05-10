49e84bcfa6d6c409c5fbbdf08818eac466018e52
Chris Murphy <christoph2005@gmail.com>
2014-05-09 23:55:22 -0700

    Less Agony?

--- dbconn.php
+++ dbconn.php
@@ -9,16 +9,11 @@
 	$host=$passAndHostExplodedURL[1];
 	$port=$portAndDatabaseExplodedURL[0];
 	$database=$portAndDatabaseExplodedURL[1];
-	echo " $user $pass $host $port $database ";
 	
-$conn = pg_connect("host=ec2-54-225-136-187.compute-1.amazonaws.com    
-
-    port=5432    
-
-    dbname=d86fo56ie0kqjt    
-
-    user=yndbtfxmnwkcgi    
-
+$conn = pg_connect(
+	"host=$host  
+    port=$o56ie0kqjt
+    user=yndbtfxmnwkcgi
     password=hiAXar8M6tn8OQNC1zrEDskrKO");
 
 function deliver_response($status, $status_message, $data){
