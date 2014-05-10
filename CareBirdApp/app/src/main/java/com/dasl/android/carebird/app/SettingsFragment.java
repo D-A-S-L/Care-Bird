a9d086f328387f309535ff5cd3284c6070f6c43e
AlecShay <alecshay@gmail.com>
2014-05-09 21:38:45 -0700

    Revert "Here is the commit where the ReminderActivity has been modified to handle incoming ReminderSchedules."
    This reverts commit bb94465f1c4a2abacc0ec37599589f8ca44c0a4e.

--- CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/SettingsFragment.java
+++ CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/SettingsFragment.java
@@ -1,65 +1,16 @@
 package com.dasl.android.carebird.app;
 
-import android.app.PendingIntent;
-import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.app.AlarmManager;
-import android.content.Intent;
-
-
-import java.util.Calendar;
-import java.util.Random;
 
 /**
  * Created by crazz_000 on 5/4/2014.
  */
 public class SettingsFragment extends PreferenceFragment {
-
-    /*
-     * For testing purposes
-     */
-    private AlarmManager keeperOfAlarms;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.mainpreferences);
-
-        // everything below here is just for testing use of Schedules
-        // can be removed later
-        ReminderSchedule a,b;
-        a = new ReminderSchedule(21, 2, "penicillin");
-        b = new ReminderSchedule(20, 4, "glucose");
-
-        keeperOfAlarms = (AlarmManager) this.getActivity().getSystemService(Context.ALARM_SERVICE);
-        exampleSchedulerMethod(a);
-        exampleSchedulerMethod(b);
-
-
-    }
-
-    public void exampleSchedulerMethod(ReminderSchedule exampleSchedule) {
-        int pseudoRand = (int) (System.currentTimeMillis() / 1000 + System.currentTimeMillis()
-                        % 1000);
-
-        Intent toReminder = new Intent(this.getActivity(), ReminderActivity.class);
-        toReminder.putExtra("REMINDER_NAME", exampleSchedule.getName());
-        toReminder.putExtra("YES_NO", exampleSchedule.getMessage());
-
-        // Calendar to get simple time of day
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(System.currentTimeMillis());
-        cal.set(Calendar.HOUR_OF_DAY, exampleSchedule.getHour());
-        cal.set(Calendar.MINUTE, exampleSchedule.getMinute());
-
-        PendingIntent temp = PendingIntent.getActivity(this.getActivity(), pseudoRand,
-                toReminder, PendingIntent.FLAG_UPDATE_CURRENT);
-        keeperOfAlarms.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY, temp);
-
-        return;
-
     }
 }
