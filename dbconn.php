23c791d36478bb4dbde1ce0446c7cc461895ffb9
Chris Murphy <christoph2005@gmail.com>
2014-05-11 18:46:50 -0700

    Anytime you use caredb.herokuapp.com/anything.php?SessionToken="someToken" it will check if someToken is a valid token, and if it is, it returns the username, if it is not then it returns null

--- dbconn.php
+++ dbconn.php
@@ -1,4 +1,5 @@
 <?php  	
+/*
 	$DATABASE_URL=getenv('DATABASE_URL');
 	$explodedURL = explode(':' , $DATABASE_URL);
 	$passAndHostExplodedURL = explode('@', $explodedURL[2]);
@@ -9,12 +10,33 @@
 	$port=$portAndDatabaseExplodedURL[0];
 	$database=$portAndDatabaseExplodedURL[1];
 	
-$conn = pg_connect(
-	"host=$host  
-    port=$port
-    user=$user
-    password=$pass
-	 dbname=$database");
+	$conn = pg_connect(
+		"host=$host  
+		 port=$port
+		 user=$user
+		 password=$pass
+		 dbname=$database"
+	);
+*/
+
+function connect() {
+$dburl;
+	if (in_array ('DATABASE_URL', $_ENV)){
+		$dburl = $_ENV['DATABASE_URL'];
+		extract(parse_url($dburl));
+		$conn=pg_connect("sslmode=require user=$user password=$pass host=$host dbname=" . substr($path, 1));
+	}
+	else{
+		$dburl="postgres://caredb:caredb@localhost:5432/cdb";
+		extract(parse_url($dburl));
+		$conn=pg_connect("user=$user password=$pass host=$host dbname=" . substr($path, 1));
+	}
+  return $conn;
+}
+$conn = connect();
+
+
+
 
 function deliver_response($status, $status_message, $data){
 	header("HTTP/1.1 $status $status_message");
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require 'login.php';
 $token = bin2hex(mcrypt_create_iv(128, MCRYPT_DEV_RANDOM));
 echo $token."\n";
-echo "Database will act strangely, I'm messing with it now.";
+echo "Not messing with db currently";
 ?>
