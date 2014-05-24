3dfd74eff1c60676ef230e42a0b6b979a9a5c215
David Scianni <dnscianni@csupomona.edu>
2014-05-23 19:49:02 -0700

    Updated Signup/Login stuff

--- CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/MainActivity.java
+++ CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/MainActivity.java
@@ -1,14 +1,20 @@
 package com.dasl.android.carebird.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +22,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        class PostTask extends AsyncTask<String, Integer, String> {
+            @Override
+            protected String doInBackground(String... params) {
+                User me = new User(params[0],params[1],params[2],params[3]);
+                com.dasl.android.carebird.app.Status response;
+                String result;
+                try {
+                    response = ((GlobalApplication) getApplication()).getDatabase().login(me);
+                    result = response.getMessage();
+                }catch (IOException error){
+                    result = "failure in try catch";
+                };
+                return result;
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+                Log.v("carebird", result);
+            }
+        }
+        new PostTask().execute(new String[]{getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("userName", "userName"), getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("password", "password"), getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("fname", "fname"), getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("lname", "lname")});
 
         // Font path
         String fontPath = "fonts/APHont-Regular_q15c.ttf";
--- CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/MainCGActivity.java
+++ CareBirdApp/app/src/main/java/com/dasl/android/carebird/app/MainCGActivity.java
@@ -1,12 +1,18 @@
 package com.dasl.android.carebird.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 /**
  * Created by David on 5/6/14.
@@ -16,6 +22,29 @@ public class MainCGActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_cg_activity);
+
+        class PostTask extends AsyncTask<String, Integer, String> {
+            @Override
+            protected String doInBackground(String... params) {
+                User me = new User(params[0],params[1],params[2],params[3]);
+                com.dasl.android.carebird.app.Status response;
+                String result;
+                try {
+                    response = ((GlobalApplication) getApplication()).getDatabase().login(me);
+                    result = response.getMessage();
+                }catch (IOException error){
+                    result = "failure in try catch";
+                };
+                return result;
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+                Log.v("carebird", result);
+            }
+        }
+        new PostTask().execute(new String[]{getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("userName", "userName"), getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("password", "password"), getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("fname", "fname"), getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getString("lname", "lname")});
 
         final Button button1 = (Button) findViewById(R.id.Schedules);
         button1.setOnClickListener(new View.OnClickListener() {
